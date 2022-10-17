c6a8604ff96cf7750cf290681a6f9346319e7b27
Scott Sutherland <scott.sutherland@gmail.com>
2022-10-17 12:36:26 -0500

    Initial import of spring initializer project

---
+++ src/main/java/net/suthsc/game/store/GravieDeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package net.suthsc.game.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravieDeveloperTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravieDeveloperTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/suthsc/game/store/GravieDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.suthsc.game.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravieDeveloperTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
