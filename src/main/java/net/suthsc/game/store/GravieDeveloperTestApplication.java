b505bcafb958a928a4d52a16a7b7c488dbeeab8c
Scott Sutherland <scott.sutherland@gmail.com>
2022-10-17 16:15:22 -0500

    Adding some deps, and component scan package

--- src/main/java/net/suthsc/game/store/GravieDeveloperTestApplication.java
+++ src/main/java/net/suthsc/game/store/GravieDeveloperTestApplication.java
@@ -2,8 +2,10 @@ package net.suthsc.game.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "net.suthsc.site")
 public class GravieDeveloperTestApplication {
 
     public static void main(String[] args) {
